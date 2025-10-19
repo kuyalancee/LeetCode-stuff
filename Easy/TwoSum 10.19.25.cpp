e77da4e50ec343f1e0bd896e86b6aa021b92f856
Lance <lancetrasporto@gmail.com>
2025-10-19 16:15:41 -0500

    Add .gitignore and TwoSum solution

---
+++ Easy/TwoSum 10.19.25.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++){
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums.at(i) + nums.at(j) == target)
+                    return {i,j};
+            }
+        }
+        return {};
+    }
+};
+
+//Time complexity is O(n^2)
+//Space complexity is O(1)
