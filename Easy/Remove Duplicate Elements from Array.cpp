c506a02da2b021e9cc9dc9ee3e1c9a8ef657863d
Lance Joseph Trasporto <LanceJosephTrasporto@my.unt.edu>
2025-10-24 21:50:08 -0500

    Add Remove duplicate elements from array solution

---
+++ Easy/Remove Duplicate Elements from Array.cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/?envType=problem-list-v2&envId=array
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int cntr = 1;
+        for(int i = 1; i < nums.size(); ){
+            if(nums.at(i) == nums.at(i-1)){
+                nums.erase(nums.begin() + i);
+            }
+            else{ 
+                cntr++;
+                i++;
+            }
+        }
+        //nums.resize(cntr);
+        return cntr;
+    }
+};
+
+//Solved 10/24/25
