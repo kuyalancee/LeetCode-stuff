6b96868070f9af676fec59481a5cf00e6fc877e5
Lance Joseph Trasporto <LanceJosephTrasporto@my.unt.edu>
2025-10-24 22:43:01 -0500

    Add Search insert position solution

---
+++ Easy/Search Insert Position.cpp
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/search-insert-position/?envType=problem-list-v2&envId=array
+#include <vector>
+using namespace std;
+
+//two pointer approach
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(target == nums.at(mid)){
+                return mid;
+            }
+            else if(target > nums.at(mid)){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return left;
+
+    }
+};
+
+//solved 10.24.25 (with AI help)
