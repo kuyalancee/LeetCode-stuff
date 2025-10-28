d2e79c4f3053a97a912dc1a81a2c435c9395b400
kuyalancee <lancetrasporto@gmail.com>
2025-10-28 17:34:47 -0500

    Add HelloWorld.js solution

---
+++ Easy/JavaScript/2667_HelloWorld.js
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/create-hello-world-function/?envType=study-plan-v2&envId=30-days-of-javascript
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+//solved 10.28
