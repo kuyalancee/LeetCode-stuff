89e537cdb4d85293237c8a6fcd624824609dad5d
kuyalancee <lancetrasporto@gmail.com>
2025-12-05 23:15:24 -0600

    Add 877 soln

---
+++ Medium/877.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/stone-game/description/
+//  #877 Leetcode
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+        //  ????
+    }
+};
